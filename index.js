c95cdaac3157fff71f3c2d271ebdfc6ac5c60f4f
yTakkar <www.shtakkar@gmail.com>
2017-12-30 21:57:25 +0530

    Changed callback to done

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function HandyCopy(options){
     var defaults = {
         selector: null,
         value: null,
-        callback: function(){ return; }
+        done: function(){ return; }
     }
     var settings = Object.assign({}, defaults, options);
     var 
         selector = settings.selector,
         value = settings.value,
-        callback = settings.callback;
+        done = settings.done;
 
     selector.on('click', function(e){
         e ? e.preventDefault() : null;
         var text = (value == null) ? $(this).data('copy') : value;
         ToClipboard(text);
-        callback();
+        done();
     });
 
 }
