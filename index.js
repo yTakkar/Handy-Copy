d3bae89557ae17ce205eb910f7ade85f1ad77f39
yTakkar <www.shtakkar@gmail.com>
2017-12-30 21:46:13 +0530

    Added callback in API

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var ToClipboard = require('./toClipboard')
 function HandyCopy(options){
     var defaults = {
         selector: null,
-        value: null
+        value: null,
+        callback: function(){ return; }
     }
     var settings = Object.assign({}, defaults, options);
     var 
         selector = settings.selector,
-        value = settings.value;
+        value = settings.value,
+        callback = settings.callback;
 
     selector.on('click', function(e){
         e ? e.preventDefault() : null;
         var text = (value == null) ? $(this).data('copy') : value;
-        ToClipboard(text)
+        ToClipboard(text);
+        callback();
     });
 
 }
